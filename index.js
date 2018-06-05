8fbc4225ea94a9e3dc99ed635145952aabf4bdda
JARVIS OLIVER <taseerrepublic@gmail.com>
2018-06-05 19:54:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,12 +16,22 @@ function logShout(string) {
 function sayHiToGdrandma (string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
-   }else if(string.toUpperCase() === string{
+   }else if(string.toUpperCase() === string){
       return "YES INDEED!" 
     } else {
         return "I love you, too"
    }
-  
+  // we need to be careful of syntax otherwise it will break
+  //we also need to use the string argument and not hardcode the text
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+   }else if(string.toUpperCase() === string){
+      return "YES INDEED!" 
+    } else {
+        return "I love you, too"
+   }
+    
+}
   
   
 
