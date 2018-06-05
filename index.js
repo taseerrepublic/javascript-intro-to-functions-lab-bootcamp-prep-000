f31e399c14c202b190e3676e295b05298987cdc5
JARVIS OLIVER <taseerrepublic@gmail.com>
2018-06-05 19:55:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,20 +21,7 @@ function sayHiToGdrandma (string) {
     } else {
         return "I love you, too"
    }
-  // we need to be careful of syntax otherwise it will break
-  //we also need to use the string argument and not hardcode the text
-  if (string.toLowerCase() === string) {
-    return "I can't hear you!"
-   }else if(string.toUpperCase() === string){
-      return "YES INDEED!" 
-    } else {
-        return "I love you, too"
-   }
-    
 }
-  
-  
-
   
  
    
