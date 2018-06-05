dee42bd25ecc3d28776624e84d2abe59f3b3fcf2
JARVIS OLIVER <taseerrepublic@gmail.com>
2018-06-05 19:57:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma (string) {
    }else if(string.toUpperCase() === string){
       return "YES INDEED!" 
     } else {
-        return "I love you, too"
+        return "I love you, too."
    }
 }
   
