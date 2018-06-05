a7b6cdbec291ee3b66c2cab75cac7ae3a1536749
JARVIS OLIVER <taseerrepublic@gmail.com>
2018-06-05 19:47:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ function logShout(string) {
  } 
 
 function sayHiToGdrandma (string) {
-  
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
    }else if(string.toUpperCase() === string{
